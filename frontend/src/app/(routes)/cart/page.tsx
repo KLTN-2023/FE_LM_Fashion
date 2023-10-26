f1a2f28c125735fac89027eecf90dfb49f0fa165
KHANH <vcatanh@gmail.com>
2023-10-26 22:05:03 +0700

    initial project structure

---
+++ frontend/src/app/(routes)/cart/page.tsx
@@ -0,0 +1,11 @@
+function Cart() {
+    return (
+        <>
+            <br></br>
+            <div>Cart</div>
+            <h1>Test</h1>
+        </>
+    )
+}
+
+export default Cart;
---
+++ frontend/src/app/(routes)/layout.tsx
@@ -0,0 +1,11 @@
+export default function CartLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }
